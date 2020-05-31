e1f6c65de8e1457d48a28f01d43da1d043ce0cc8
Shanmugapriyan <shanmugapriyan9696@gmail.com>
2020-06-01 00:08:14 +0530

    Added common method

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import requests
-from bs4 import BeautifulSoup
 from pprint import pprint
 
+import requests
+from bs4 import BeautifulSoup
 
 url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
-page = requests.get(url, headers = headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_content(url):
+    return BeautifulSoup(requests.get(url, headers=HEADERS).content, 'html.parser')
+
+
+soup = get_content(url)
 movie_tags = soup.findAll('td', {'class': 'titleColumn'})
 
 movie_ids = {}
@@ -23,13 +27,9 @@ reviewURL = 'https://www.imdb.com/title/{id}/reviews?ref_=tt_urv'
 reviews = {}
 i = 0
 for title, movie_id in movie_ids.items():
-    page = requests.get(reviewURL.replace('{id}', movie_id), headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = get_content(reviewURL.replace('{id}', movie_id))
     reviews[title] = soup.find('div', {'class': 'text show-more__control'}).get_text()
     i += 1
-    if(i == 25):
+    if (i == 25):
         break
 pprint(reviews)
-
-
-
