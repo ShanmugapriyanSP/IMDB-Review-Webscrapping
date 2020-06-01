d9306bf260585bbee0eb68deba56aa4f9ebb0255
Shanmugapriyan <shanmugapriyan9696@gmail.com>
2020-06-02 02:17:01 +0530

    Config file added, Multi processing implemented

---
+++ config.py
@@ -0,0 +1,5 @@
+URL = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/83.0.4103.61 Safari/537.36'}
+REVIEW_URL = 'https://www.imdb.com/title/{id}/reviews?ref_=tt_urv'
--- main.py
+++ main.py
@@ -1,35 +1,44 @@
+from datetime import datetime
+from multiprocessing import Pool
 from pprint import pprint
 
+import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
+import config
 
 
-def get_content(url):
-    return BeautifulSoup(requests.get(url, headers=HEADERS).content, 'html.parser')
+def get_content(url1):
+    return BeautifulSoup(requests.get(url1, headers=config.HEADERS).content, 'html.parser')
 
 
-soup = get_content(url)
-movie_tags = soup.findAll('td', {'class': 'titleColumn'})
+def get_reviews(movie):
+    review = {}
+    title, id = movie.split("---")
+    soup = get_content(config.REVIEW_URL.replace('{id}', id))
+    try:
+        review[title] = soup.find('div', {'class': 'text show-more__control'}).get_text()
+    except AttributeError:
+        review[title] = np.NaN
+    print(title)
+    return review
 
-movie_ids = {}
-for id in movie_tags:
-    value = id.find('a')
-    movie_ids[value.get_text()] = str(value)[16:25]
 
-# print(movie_ids)
+if __name__ == '__main__':
+    start = datetime.now()
 
-reviewURL = 'https://www.imdb.com/title/{id}/reviews?ref_=tt_urv'
-reviews = {}
-i = 0
-for title, movie_id in movie_ids.items():
-    soup = get_content(reviewURL.replace('{id}', movie_id))
-    reviews[title] = soup.find('div', {'class': 'text show-more__control'}).get_text()
-    i += 1
-    if (i == 25):
-        break
-pprint(reviews)
+    soup = get_content(config.URL)
+    movie_tags = soup.findAll('td', {'class': 'titleColumn'})
+
+    movie_ids = []
+    for id in movie_tags:
+        value = id.find('a')
+        movie_ids.append(value.get_text() + '---' + (str(value)[16:25]))
+
+    pprint(movie_ids)
+    pool = Pool()
+    reviews = pool.map(get_reviews, movie_ids)
+    pprint(reviews)
+    finish = datetime.now()
+    print(finish - start)
