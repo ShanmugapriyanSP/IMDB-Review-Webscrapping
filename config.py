3e73da953ed072b2b709d28152d3f149c14e992a
Shanmugapriyan <shanmugapriyan9696@gmail.com>
2020-06-03 03:19:00 +0530

    Committing changed files

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-URL = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+SERIES_HOME_URL = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
+MOVIES_HOME_URL = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/83.0.4103.61 Safari/537.36'}
--- main.py
+++ main.py
@@ -8,37 +8,44 @@ from bs4 import BeautifulSoup
 
 import config
 
+reviews = {}
+
 
 def get_content(url1):
     return BeautifulSoup(requests.get(url1, headers=config.HEADERS).content, 'html.parser')
 
 
+def get_all_reviews(review, title):
+    reviews[title] = reviews[title] + '||' + review.getText()
+
+
 def get_reviews(movie):
-    review = {}
-    title, id = movie.split("---")
+    title, id = movie.split("|")
     soup = get_content(config.REVIEW_URL.replace('{id}', id))
     try:
-        review[title] = soup.find('div', {'class': 'text show-more__control'}).get_text()
-    except AttributeError:
-        review[title] = np.NaN
+        review_list = soup.find_all('div', {'class': 'text show-more__control'})
+        print(review_list.size())
+        for review in review_list:
+            reviews[title] = reviews[title] + '||' + review.getText()
+    except (AttributeError, KeyError) as e:
+        reviews[title] = reviews[title] + '||' + np.NaN
     print(title)
-    return review
 
 
 if __name__ == '__main__':
     start = datetime.now()
 
-    soup = get_content(config.URL)
+    soup = get_content(config.MOVIES_HOME_URL)
     movie_tags = soup.findAll('td', {'class': 'titleColumn'})
 
     movie_ids = []
     for id in movie_tags:
         value = id.find('a')
-        movie_ids.append(value.get_text() + '---' + (str(value)[16:25]))
+        movie_ids.append(value.get_text() + '|' + (str(value)[16:25]))
 
     pprint(movie_ids)
     pool = Pool()
-    reviews = pool.map(get_reviews, movie_ids)
+    pool.map(get_reviews, movie_ids)
     pprint(reviews)
     finish = datetime.now()
     print(finish - start)
