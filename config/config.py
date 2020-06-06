d290f46a8f8dd780144303b3ed82ac9830ec3bf2
Shanmugapriyan <shanmugapriyan9696@gmail.com>
2020-06-07 00:21:17 +0530

    Added ascending reviews, lowest rated movies & series

--- config/config.py
+++ config/config.py
@@ -1,12 +1,18 @@
-SERIES_HOME_URL = 'https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250'
+SERIES_TOP_URL = 'https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250'
 
-MOVIES_HOME_URL = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+MOVIES_TOP_URL = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+
+SERIES_BOTTOM_URL = 'https://www.imdb.com/list/ls063837343/?sort=list_order,asc&st_dt=&mode=simple&page=1&ref_=ttls_vw_smp'
+
+MOVIES_BOTTOM_URL = 'https://www.imdb.com/list/ls031780759/?sort=list_order,asc&st_dt=&mode=simple&page=1&ref_=ttls_vw_smp'
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/83.0.4103.61 Safari/537.36'}
 
-REVIEW_URL = 'https://www.imdb.com/title/{id}/reviews?ref_=tt_urv'
+GOOD_REVIEW_URL = 'https://www.imdb.com/title/{id}/reviews?ref_=tt_urv'
+
+BAD_REVIEW_URL = 'https://www.imdb.com/title/{id}/reviews?sort=userRating&dir=asc&ratingFilter=0'
 
 MONGO_DB_CLIENT = "mongodb://localhost:27017/"
 
--- movie_review_webscrapping/main.py
+++ movie_review_webscrapping/main.py
@@ -34,13 +34,16 @@ class Review:
         self.reviews = []
         self.dict_list = []
 
-    def load_tags(self):
-        self.tags = self.soup.findAll('td', {'class': 'titleColumn'})
+    def load_tags(self, rated='top'):
+        if rated == 'top':
+            self.tags = self.soup.findAll('td', {'class': 'titleColumn'})
+        elif rated == 'bottom':
+            self.tags = self.soup.findAll('div', {'class': 'col-title'})
 
     def load_imdb_ids(self):
-        for id in self.tags:
+        for imdb_id in self.tags:
             dict = {}
-            value = id.find('a')
+            value = imdb_id.find('a')
             self.ids.append(value.get_text() + '|' + (str(value)[16:25]))
             dict['title'] = value.get_text()
             dict['imdb_id'] = str(value)[16:25]
@@ -53,27 +56,43 @@ def get_reviews(ids):
     review = {}
     try:
         title, imdb_id = ids.split("|")
-        review_url = config.REVIEW_URL.replace('{id}', imdb_id)
-        soup = get_soup(review_url)
-        review_list = [element.get_text() for element in
-                       soup.find_all('div', {'class': 'text show-more__control'})]
-        page = requests.get(review_url)
-        rating_list = [get_text_from_xpath(page, f'(//span[@class="rating-other-user-rating"]//span[1])[{i}]')
-                       for i in range(0, len(review_list) + 1)]
+
+        good_review_url = config.GOOD_REVIEW_URL.replace('{id}', imdb_id)
+        bad_review_url = config.BAD_REVIEW_URL.replace('{id}', imdb_id)
+
+        good_soup = get_soup(good_review_url)
+        good_review_list = [element.get_text() for element in
+                            good_soup.find_all('div', {'class': 'text show-more__control'})]
+        good_page = requests.get(good_review_url)
+        good_rating_list = [get_text_from_xpath(good_page, f'(//span[@class="rating-other-user-rating"]//span[1])[{i}]')
+                            for i in range(0, len(good_review_list))]
+
+        bad_soup = get_soup(bad_review_url)
+        bad_review_list = [element.get_text() for element in
+                           bad_soup.find_all('div', {'class': 'text show-more__control'})]
+        bad_page = requests.get(bad_review_url)
+        bad_rating_list = [get_text_from_xpath(bad_page, f'(//span[@class="rating-other-user-rating"]//span[1])[{i}]')
+                           for i in range(0, len(bad_review_list))]
+
         review['name'] = title
-        review['review'] = review_list
-        review['ratings'] = rating_list
+        review['review'] = good_review_list
+        review['review'].extend(bad_review_list)
+        review['ratings'] = good_rating_list
+        review['ratings'].extend(bad_rating_list)
         config.logger.info('Title =====>' + title)
-        config.logger.info('Total Reviews =====>' + str(len(review_list)))
-        config.logger.info('Total Reviews =====>' + str(len(rating_list)))
+        config.logger.info('Good Reviews =====>' + str(len(good_review_list)))
+        config.logger.info('Bad Reviews =====>' + str(len(bad_review_list)))
+        config.logger.info('Good Rating =====>' + str(len(good_rating_list)))
+        config.logger.info('Bad Rating =====>' + str(len(bad_rating_list)))
     except (AttributeError, KeyError) as e:
         config.logger.error('Error has occurred in get_reviews =====>' + e)
         review['name'] = title
-        review['review'] = np.NaN
+        review['review'].extend(np.NaN)
+        review['ratings'].extend(np.NaN)
     return review
 
 
-def run(review):
+def fetch_reviews(review):
     pool = Pool(processes=multiprocessing.cpu_count())
     rs = pool.map(get_reviews, review.ids)
     review.reviews = rs
@@ -84,22 +103,34 @@ def run(review):
 def start():
     start_time = datetime.now()
 
-    movies = Review(config.MOVIES_HOME_URL)
-    series = Review(config.SERIES_HOME_URL)
+    movies_top = Review(config.MOVIES_TOP_URL)
+    series_top = Review(config.SERIES_TOP_URL)
+    movies_bottom = Review(config.MOVIES_BOTTOM_URL)
+    series_bottom = Review(config.SERIES_BOTTOM_URL)
 
-    movies.load_tags()
-    series.load_tags()
+    movies_top.load_tags('top')
+    series_top.load_tags('top')
+    movies_bottom.load_tags('bottom')
+    series_bottom.load_tags('bottom')
 
-    movies.load_imdb_ids()
-    series.load_imdb_ids()
+    movies_top.load_imdb_ids()
+    series_top.load_imdb_ids()
+    movies_bottom.load_imdb_ids()
+    series_bottom.load_imdb_ids()
 
-    run(movies)
-    run(series)
+    fetch_reviews(movies_top)
+    fetch_reviews(series_top)
+    fetch_reviews(movies_bottom)
+    fetch_reviews(series_bottom)
 
-    data_storage.store_data(movies.dict_list, 'imdb_movie_id')
-    data_storage.store_data(movies.reviews, 'movie_reviews')
-    data_storage.store_data(series.dict_list, 'imdb_series_id')
-    data_storage.store_data(series.reviews, 'series_reviews')
+    data_storage.store_data(movies_top.dict_list, 'imdb_movie_id')
+    data_storage.store_data(movies_top.reviews, 'movie_reviews')
+    data_storage.store_data(series_top.dict_list, 'imdb_series_id')
+    data_storage.store_data(series_top.reviews, 'series_reviews')
+    data_storage.store_data(movies_bottom.dict_list, 'imdb_movie_id')
+    data_storage.store_data(movies_bottom.reviews, 'movie_reviews')
+    data_storage.store_data(series_bottom.dict_list, 'imdb_series_id')
+    data_storage.store_data(series_bottom.reviews, 'series_reviews')
 
     finish_time = datetime.now()
 
